36aab4bf6412a99a285280594740edd6caa755e9
JOJOK63 <77489999+JOJOK63@users.noreply.github.com>
2025-06-06 13:29:43 +0200

    project front ok

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,55 @@
+import { NgFor } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [NgFor],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css',
+  standalone: true,
+})
+export class NavbarComponent {
+  themes = [
+    'light',
+    'dark',
+    'cupcake',
+    'bumblebee',
+    'emerald',
+    'corporate',
+    'synthwave',
+    'retro',
+    'cyberpunk',
+    'valentine',
+    'halloween',
+    'garden',
+    'forest',
+    'aqua',
+    'lofi',
+    'pastel',
+    'fantasy',
+    'wireframe',
+    'black',
+    'luxury',
+    'dracula',
+    'cmyk',
+    'autumn',
+    'business',
+    'acid',
+    'lemonade',
+    'night',
+    'coffee',
+    'winter',
+    'dim',
+    'nord',
+    'sunset',
+  ];
+
+  setTheme(theme: string | Event) {
+    if (theme instanceof Event) {
+      const select = theme.target as HTMLSelectElement;
+      theme = select.value;
+    }
+
+    document.documentElement.setAttribute('data-theme', theme);
+  }
+}
