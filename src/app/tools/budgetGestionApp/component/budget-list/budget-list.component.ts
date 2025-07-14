cf808dadd6bb04d68fa221f976db816466aa0e2c
JOJOK63 <77489999+JOJOK63@users.noreply.github.com>
2025-07-14 20:27:11 +0200

    page budget

--- src/app/tools/budgetGestionApp/component/budget-list/budget-list.component.ts
+++ src/app/tools/budgetGestionApp/component/budget-list/budget-list.component.ts
@@ -1,22 +1,34 @@
 import {Component, OnInit} from '@angular/core';
 import {Budget} from '../../model/Budget';
 import {BudgetService} from '../../service/budget.service';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-budget-list',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './budget-list.component.html',
   styleUrl: './budget-list.component.css'
 })
 export class BudgetListComponent implements OnInit {
   budgetLists: Budget[] = [];
+  selectedBudgetId: number| null = null;
+  selectedBudget: Budget | undefined = undefined;
 
   constructor(private budgetService: BudgetService) {}
 
 
   ngOnInit( ) {
-      this.budgetService.getAllBudgets();
       this.budgetLists = this.budgetService.getAllBudgets();
       console.log(this.budgetLists);
+  }
+
+  onBudgetChange() {
+    if(this.selectedBudgetId){
+      this.selectedBudget = this.budgetLists.find(budget => budget.id == this.selectedBudgetId)
+    }else{
+      this.selectedBudget = undefined
+    }
   }
 }
